a797fb18abe81b1891ac4640cf4dae22e5ef75cd
tlad <tlad@trask.cz>
2022-05-05 10:52:00 +0200

    a

--- example/src/main/java/io/github/tomaslad/example/Main.java
+++ example/src/main/java/io/github/tomaslad/example/Main.java
@@ -1,27 +1,9 @@
 package io.github.tomaslad.example;
 
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.*;
-
-import java.io.IOException;
-import java.util.List;
-
 class Main {
 
-    public static void main(String args[]) throws GitAPIException, IOException {
-        Repository repository = new RepositoryBuilder().readEnvironment().findGitDir().build();
-        ObjectId objectId = repository.resolve(Constants.HEAD);
-        String tagName = getTag(repository);
+    public static void main(String args[]) {
+        System.out.println("Ahoj");
     }
 
-    private static String getTag(Repository repo) throws GitAPIException {
-        try (Git git = Git.wrap(repo)) {
-            List<Ref> refs = git.tagList().call();
-            for (Ref ref : refs) {
-                System.out.println("ref: " + ref.getName());
-            }
-            return "ahoj";
-        }
-    }
 }
