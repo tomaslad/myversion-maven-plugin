5cd2f15b294784398d48a7c70dca88125c79255c
tlad <tlad@trask.cz>
2022-05-05 21:21:04 +0200

    a

--- src/main/java/io/github/tomaslad/maven/plugin/myversion/GitUtils.java
+++ src/main/java/io/github/tomaslad/maven/plugin/myversion/GitUtils.java
@@ -2,28 +2,32 @@ package io.github.tomaslad.maven.plugin.myversion;
 
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.buildobjects.process.ExternalProcessFailureException;
 import org.buildobjects.process.ProcBuilder;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @UtilityClass
 public class GitUtils {
 
-    public static void readLatestTag() {
-        ProcBuilder procBuilder = new ProcBuilder("git describe") // https://git-scm.com/docs/git-describe
-                .withArg("--tags")
-                .withArg("--always")
-                .withArg("--first-parent")
-                .withArg("--match=v*")
-                .withArg("--abbrev=0")
-                .withArg("--dirty");
+    public static String readLatestTag() {
+        // https://git-scm.com/docs/git-describe
 
+
+
+       // ProcessBuilder processBuilder = new ProcessBuilder().command("git describe --tags --always --first-parent --match=v* --abbrev=0 --dirty");
         try {
-            procBuilder.run();
-        } catch (ExternalProcessFailureException e) {
-            log.error("Read latest tag error: ", e);
+            Process process = Runtime.getRuntime().exec("git describe --tags --always --first-parent --match=v* --abbrev=0 --dirty");
+           // Process process = processBuilder.start();
+            int exitValue = process.waitFor();
+            log.info("exitValue: {}", exitValue);
+            return IOUtils.toString(process.getInputStream(), StandardCharsets.UTF_8);
+        } catch (IOException | InterruptedException e) {
+            log.error("Set tag error: ", e);
+            throw new RuntimeException(e);
         }
     }
 
--- src/test/java/io/github/tomaslad/maven/plugin/myversion/GitUtilsTest.java
+++ src/test/java/io/github/tomaslad/maven/plugin/myversion/GitUtilsTest.java
@@ -1,13 +1,18 @@
 package io.github.tomaslad.maven.plugin.myversion;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import static com.google.common.truth.Truth.assertThat;
 
+@Slf4j
 public class GitUtilsTest {
 
     @Test
     public void test() {
+        String s = GitUtils.readLatestTag();
+        System.out.println("S: " + s);
+
         GitUtils.writeTag2("vAhoj");
     }
 }
