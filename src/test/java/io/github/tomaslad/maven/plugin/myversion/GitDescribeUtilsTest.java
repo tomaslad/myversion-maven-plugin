44ebb174f0c718a3dd8c66360dbda22d919e7631
mmatousek <mmatousek@trask.cz>
2022-05-10 08:32:35 +0200

    parameterized tests

--- src/test/java/io/github/tomaslad/maven/plugin/myversion/GitDescribeUtilsTest.java
+++ src/test/java/io/github/tomaslad/maven/plugin/myversion/GitDescribeUtilsTest.java
@@ -3,7 +3,8 @@ package io.github.tomaslad.maven.plugin.myversion;
 import io.github.tomaslad.maven.plugin.myversion.git.GitDescribe;
 import io.github.tomaslad.maven.plugin.myversion.git.GitDescribeUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,30 +13,34 @@ class GitDescribeUtilsTest {
 
     GitDescribe.GitDescribeBuilder builder = GitDescribe.builder();
 
-    @Test
-    public void parseVersion1() {
-        assertEquals(builder.tagName("v1.0.0rc.1").commitDistance(5).commitAbbrev("5cd2f15").dirty(true).build(),
-                GitDescribeUtils.parse("v1.0.0-rc.1-5-g5cd2f15-dirty"));
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, Integer.MAX_VALUE})
+    public void parseCommitDistanceNonZero(int number) {
+        assertEquals(builder.tagName("v1.0.0rc.1").commitDistance(number).commitAbbrev("5cd2f15").dirty(true).build(),
+                GitDescribeUtils.parse(String.format("v1.0.0-rc.1-%s-g5cd2f15-dirty", number)));
     }
 
-    @Test
-    public void parseVersion2() {
-        assertEquals(builder.tagName("v1.0.0rc.1").dirty(true).build(),
-                GitDescribeUtils.parse("v1.0.0-rc.1-dirty"));
-
-    }
-
-    @Test
-    public void parseVersion3() {
-        assertEquals(builder.tagName("v2.0.0").commitDistance(2).commitAbbrev("3cd23f6").build(),
-                GitDescribeUtils.parse("v2.0.0-2-g3cd23f6"));
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, Integer.MAX_VALUE})
+    public void parseParameterizedRc(int number) {
+        assertEquals(builder.tagName(String.format("v1.0.0rc.%s", number)).dirty(true).build(),
+                GitDescribeUtils.parse(String.format("v1.0.0-rc.%s-dirty", number)));
 
     }
 
-    @Test
-    public void parseVersion4() {
-        assertEquals(builder.tagName("v3.1.3").build(),
-                GitDescribeUtils.parse("v3.1.3"));
+    @ParameterizedTest
+    @ValueSource(ints = 0)
+    public void parseCommitDistanceZero(int number) {
+        assertEquals(builder.tagName("v2.0.0").commitDistance(number).commitAbbrev("3cd23f6").build(),
+                GitDescribeUtils.parse("v2.0.0-g3cd23f6"));
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, Integer.MAX_VALUE})
+    public void parseParameterizedVersions(int number) {
+        assertEquals(builder.tagName(String.format("v1$s.1$s.1$s", number)).build(),
+                GitDescribeUtils.parse(String.format("v1$s.1$s.1$s", number)));
 
     }
 }
