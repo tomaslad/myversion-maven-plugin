3cd1f92c0d9653dfe7400539793290c036f0ae7f
mmatousek <mmatousek@trask.cz>
2022-05-15 16:16:12 +0200

    add uncommitedChanges check before bump

--- src/main/java/io/github/tomaslad/maven/plugin/myversion/AbstractBumpMojo.java
+++ src/main/java/io/github/tomaslad/maven/plugin/myversion/AbstractBumpMojo.java
@@ -4,17 +4,23 @@ import io.github.tomaslad.maven.plugin.myversion.git.GitDescribe;
 import io.github.tomaslad.maven.plugin.myversion.git.GitUtils;
 import io.github.tomaslad.maven.plugin.myversion.semver.SemVer;
 import io.github.tomaslad.maven.plugin.myversion.semver.SemVerUtils;
+import lombok.SneakyThrows;
 import org.apache.maven.plugin.AbstractMojo;
 
 public abstract class AbstractBumpMojo extends AbstractMojo {
 
+    @SneakyThrows
     @Override
     public void execute() {
-        GitDescribe describe = GitUtils.describe();
-        SemVer semVer1 = SemVerUtils.parse(describe.getTagName());
-        SemVer semVer2 = bump(semVer1);
-        getLog().info("Bump version from " + semVer1 + " to " + semVer2);
-        GitUtils.tag(semVer2.toString());
+        if (System.getProperty("checkUncommitedChanges").equals("1") && GitUtils.hasUncommitedChanges()) {
+            getLog().error("Repository has uncommited changes");
+        } else {
+            GitDescribe describe = GitUtils.describe();
+            SemVer semVer1 = SemVerUtils.parse(describe.getTagName());
+            SemVer semVer2 = bump(semVer1);
+            getLog().info("Bump version from " + semVer1 + " to " + semVer2);
+            GitUtils.tag(semVer2.toString());
+        }
     }
 
     protected abstract SemVer bump(SemVer semVer);
